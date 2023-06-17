b75345287b2a270595c41205cdfe3c4b35b72de2
nukesaku <nextniconico@gmail.com>
2023-06-17 18:44:31 +0900

    [section6]finished. without multi page.

---
+++ section6/test/src/modules/TestComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="red-b">
+    <div class="border__blue">
+      SCSSテストです
+    </div>
+    <div class="background__primary">
+      SCSSグローバルテストです
+    </div>
+    テストです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TestComponent',
+  data(){
+    return{
+      testData:'テストdataです'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.red-b{
+  border:1px red solid;
+}
+
+.border{
+  &__blue{
+    border:1px blue solid;
+  }
+}
+
+.background__primary{
+  background-color: $color-primary;
+}
+</style>
--- section6/test/vue.config.js
+++ section6/test/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: '',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/sass/main.scss";`
+      }
+    }
+  }
+}
